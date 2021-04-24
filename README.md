f66e5952c81de7b85e2c5209f1ef7cb5eafd3c29
smhagit <mail@sascha-reule.de>
2021-04-24 19:34:27 +0200

    change(): added a loop with configurable interval

--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 ![Docker Image CI](https://github.com/smhagit/homeip/actions/workflows/docker-image.yml/badge.svg?branch=master)
 
 This is a small alpine based nodejs container which will gather your public IP from a cloud service. This IP is then used to update a DNS record via Hetzner DNS API 
+
+
+## Usage
+
+**Attention: This only works with Hetzner DNS Api.
+
+`docker-compose.yml`  
+```yaml
+version: "3.9"
+services:
+  app:
+    image: smhagit/homeip
+  environment:
+    HETZNER_RECORD_ID: xxx
+    HETZNER_ZONE_ID: xxx
+    HETZNER_RECORD_NAME: home # xxx.domain.tld
+    HETZNER_API_TOKEN: xxx
+```
--- build/app/src/index.js
+++ build/app/src/index.js
@@ -5,35 +5,52 @@ if (!process.env.HETZNER_RECORD_ID || !process.env.HETZNER_ZONE_ID || !process
     process.exit(0);
 }
 
-request('https://api.ipify.org', (error, response, publicIP) => {
-  if (!error && response.statusCode == 200) {
-    console.log(`gathered public IP: ${publicIP}`);
 
-    // let's update hetzner dns
-    request({
-        method: 'PUT',
-        url: `https://dns.hetzner.com/api/v1/records/${process.env.HETZNER_RECORD_ID}`,
-        json: {
-            value: publicIP,
-            ttl: process.env.HETZNER_RECORD_TTL || 3600,
-            type: process.env.HETZNER_RECORD_TYPE || "A",
-            name: process.env.HETZNER_RECORD_NAME,
-            zone_id: process.env.HETZNER_ZONE_ID
-        },
-        headers: {
-            'Content-Type': 'application/json',
-            'Auth-API-Token': process.env.HETZNER_API_TOKEN
-        }},
-        (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                console.log(`updated hetzner dns`);
-            } else {
-                console.log(`error while updating hetzner dns: ${response.statusCode}`);
+function checkAndUpdate() {
+    request('https://api.ipify.org', (error, response, publicIP) => {
+        if (!error && response.statusCode == 200) {
+        console.log(`gathered public IP: ${publicIP}`);
+    
+        // let's update hetzner dns
+        request({
+            method: 'PUT',
+            url: `https://dns.hetzner.com/api/v1/records/${process.env.HETZNER_RECORD_ID}`,
+            json: {
+                value: publicIP,
+                ttl: process.env.HETZNER_RECORD_TTL || 3600,
+                type: process.env.HETZNER_RECORD_TYPE || "A",
+                name: process.env.HETZNER_RECORD_NAME,
+                zone_id: process.env.HETZNER_ZONE_ID
+            },
+            headers: {
+                'Content-Type': 'application/json',
+                'Auth-API-Token': process.env.HETZNER_API_TOKEN
+            }},
+            (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    console.log(`updated hetzner dns`);
+                } else {
+                    console.log(`error while updating hetzner dns: ${response.statusCode}`);
+                }
             }
+        );
         }
-    );
-  }
-  else {
-    console.log(`error gathering public ip: ${response.statusCode}`);
-  }
-});
+        else {
+        console.log(`error gathering public ip: ${response.statusCode}`);
+        }
+    });
+}
+
+
+function sleep(sec) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, parseInt(sec) * 1000);
+    });
+}
+
+
+
+do {
+    checkAndUpdate();
+    await sleep(process.env.INTERVAL || 5400);
+} while (true);
